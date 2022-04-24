eb08e1ea5da4dd358693b4603a9abd2fcf9bb58b
이동준 <bomul1128@naver.com>
2022-04-25 02:11:38 +0900

    풀이 간소화

--- 0421/bomul1128/[BOJ]_9012_괄호.py
+++ 0421/bomul1128/[BOJ]_9012_괄호.py
@@ -1,15 +1,18 @@
-a = int(input())
-for _ in range(a):
-    inp = input()
+from sys import stdin
+
+input = stdin.readline
+
+for _ in range(int(input())):
+    arr = input().strip()
     lr = 0
     result = 'YES'
-    for i in inp:
+    for i in arr:
         if i == '(':
             lr += 1
         else:
             lr -= 1
-        if lr < 0:
-            break
-    if lr != 0:
+            if lr < 0:
+                break
+    if lr:
         result = 'NO'
     print(result)
